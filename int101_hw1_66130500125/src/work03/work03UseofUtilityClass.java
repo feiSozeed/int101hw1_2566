706b286699e92d124202e03c2de7e8c5f143d597
Chanombude <137422903+feiSozeed@users.noreply.github.com>
2023-09-24 21:23:21 +0700

    Delete int101_hw1_66130500125/src/work03 directory

--- int101_hw1_66130500125/src/work03/work03UseofUtilityClass.java
+++
@@ -1,7 +0,0 @@
-import MyUtil;
-public class work03UseofUtilityClass {
-    public static void main(String[] args) {
-        double bmi = calculateBMI(80,170);
-        System.out.println(bmi);
-    }
-}
